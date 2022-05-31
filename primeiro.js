0487e326d3f63d6136b778c5b87edfbec24b0237
Gabriela Tavares <gabriela.tavares0095@gmail.com>
2022-05-31 13:59:44 -0300

    first commit

---
+++ primeiro.js
@@ -0,0 +1,9 @@
+const nome = "Gabriela";
+const idade = 16;
+const altura =1.62;
+let verdadeiro= true;
+let falso = false;
+
+console.log("meu nome e " + nome+ " "+ " e eu tenho "+ Idade+" anos, é minha altura é "+ altura);
+
+console.log("Terça é dia de assistir masterchef "+falso)
